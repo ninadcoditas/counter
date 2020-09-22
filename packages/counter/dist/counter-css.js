716b5f7c9485dfcb39f317e56cff95ce8a797470
ninad <ninad.virkar@coditas.com>
2020-09-22 15:12:54 +0530

    added dist

---
+++ packages/counter/dist/counter-css.js
@@ -0,0 +1,63 @@
+import { css } from 'lit-element';
+export default css `
+:host{
+  --counter-background-color: var(--background-01);
+  --counter-disabled-color: var(--disabled-text);
+  --counter-enabled-state:var(--secondary);
+  --counter-font-family: var(--primary-font-family);
+  --counter-font-color:	var(--primary-text);
+  --counter-supporting-font-family: var(--primary-font-family);
+  --counter-supporting-label-color:var(--secondary-text);
+    --counter-opacity:100%;
+    --counter-font-size:1em;
+    --shadow:none;
+    --counter-font-weight:semibold;
+    --counter-supporting-label-size:1.4em;      
+    --counter-supporting-label-weight:regular;
+
+}
+
+.counter{
+    display:flex;
+    justify-content:space-between;
+}
+.counter_label{
+    font-size:2em;
+    color:var( --counter-supporting-label-color);
+    font-weight:var(--counter-supporting-label-weight);
+}
+
+.counter_container{
+    display: flex;
+    width: 120px;
+    // background-color: var(--counter-background-color);
+    opacity: var(--counter-opacity);
+    font-family: var(--counter-font-family);
+    color:var(--counter-font-color);
+    font-size: var(--counter-font-size);
+    font-weight: var(--counter-font-weight);
+}
+
+.counter_element{
+    width: 40px;
+    height: 40px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    font-size:2em;
+}
+button{
+    width: 100%;
+    height: 100%;
+    font-size:var(--counter-supporting-label-size);
+    color:var(--counter-enabled-state);
+}
+button:disabled{
+    color:var(--counter-disabled-color);
+} 
+
+.hide{
+    display:none;
+}
+
+`;
---
+++ packages/counter/dist/orxe-counter.js
@@ -0,0 +1,97 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { html, customElement, LitElement, property } from 'lit-element';
+import styles from './counter-css';
+let OrxeCounter = class OrxeCounter extends LitElement {
+    constructor() {
+        super();
+        this.value = 0;
+        this.min = 0;
+        this.max = 0;
+        this.value = parseInt(this.getAttribute("value").toString());
+        this.min = parseInt(this.getAttribute("min").toString());
+        this.max = parseInt(this.getAttribute("max").toString());
+    }
+    render() {
+        return html `
+  <div class="counter">
+
+  ${this.innerHTML.trim() != "" ?
+            html ` 
+        <div class="counter_label"> 
+          <slot></slot>
+          </div>` :
+            html ``}
+
+    <div class="counter_container">
+    <div class="counter_element">
+    <button @click="${this.decrement}" id="decrement"> 
+    - 
+    </button>
+    </div>  
+    <div class="counter_element">${this.value}</div>
+    <div class="counter_element">
+    <button @click="${this.increment}" id="increment">
+    + 
+    </button>
+    </div>
+    </div>
+    </div>
+    `;
+    }
+    decrement() {
+        this.value--;
+        this.handleEdgeCase();
+    }
+    increment() {
+        this.value++;
+        this.handleEdgeCase();
+    }
+    handleEdgeCase() {
+        let incrementButton = this.shadowRoot.getElementById("increment");
+        let decrementButton = this.shadowRoot.getElementById("decrement");
+        if (this.value >= this.max) {
+            incrementButton.disabled = true;
+        }
+        else {
+            incrementButton.disabled = false;
+        }
+        if (this.value <= this.min) {
+            decrementButton.disabled = true;
+        }
+        else {
+            decrementButton.disabled = false;
+        }
+        let event = new CustomEvent('value-changed', {
+            detail: {
+                value: this.value
+            }
+        });
+        this.dispatchEvent(event);
+    }
+};
+OrxeCounter.styles = styles;
+__decorate([
+    property({ type: Number }),
+    __metadata("design:type", Object)
+], OrxeCounter.prototype, "value", void 0);
+__decorate([
+    property({ type: Number }),
+    __metadata("design:type", Object)
+], OrxeCounter.prototype, "min", void 0);
+__decorate([
+    property({ type: Number }),
+    __metadata("design:type", Object)
+], OrxeCounter.prototype, "max", void 0);
+OrxeCounter = __decorate([
+    customElement('orxe-counter'),
+    __metadata("design:paramtypes", [])
+], OrxeCounter);
+export default OrxeCounter;
