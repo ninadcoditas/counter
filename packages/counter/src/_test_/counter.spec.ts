7ac52bfefa5079c68fdc47510d22b5bf5c05c8d2
ninad <ninad.virkar@coditas.com>
2020-09-24 13:12:49 +0530

    tests added

--- packages/counter/src/_test_/counter.spec.ts
+++ packages/counter/src/_test_/counter.spec.ts
@@ -30,9 +30,6 @@ describe('orxe-counter', () => {
   }
 
   function getValue() {
-    // let counterValue = getElement('#value').innerHTML.trim()
-    // counterValue = counterValue.replace(/<\!--.*?-->/g, "");
-    // return parseInt(counterValue);
     return counterElement.value
   }
 
@@ -63,8 +60,6 @@ describe('orxe-counter', () => {
   it('should disable + button when value >= max value ', () => {
     let currentValue = getValue();
     if (currentValue >= counterElement.max) {
-      console.log('curr ', currentValue);
-      console.log('max ', counterElement.max);
       expect(getElement('#increment').hasAttribute('disabled')).toBeTruthy()
     }
   })
@@ -75,40 +70,4 @@ describe('orxe-counter', () => {
       expect(getElement('#decrement').hasAttribute('disabled')).toBeTruthy()
     }
   })
-
-  // function getByTestId(id: string): HTMLElement {
-  //   return radioButton.shadowRoot.querySelector(`[data-testid=${id}]`);
-  // }
-  // it('should check default value for properties for radio button card', () => {
-  //   expect(radioButton.radioCheckedFlag).toBeFalsy();
-  //   expect(radioButton.radioLable).toEqual('');
-  //   expect(radioButton.radioValue).toEqual('');
-  // });
-
-  // it('has checked content class', () => {
-  //   const checkedContent = getByTestId('radio-button-card-checked-content');
-  //   expect(checkedContent).toHaveClass('checked-content');
-  // });
-  // it('check css value for checked content before radio button check', () => {
-  //   const beforeRadioButtonCheck = getByTestId('radio-button-card-checked-content');
-  //   expect(beforeRadioButtonCheck).toHaveStyle({
-  //     backgroundColor: window.getComputedStyle(document.body).getPropertyValue('--global-bg')
-  //   });
-  // });
-  // it('radio button default unchecked', () => {
-  //   const radioInput = getByTestId('radio-button-card-radio-input');
-  //   expect(radioInput).not.toBeChecked()
-  // });
-  // it('check on click radio button status and css value', async () => {
-  //   const radioInputClick = getByTestId('radio-button-card-radio-input');
-  //   await radioInputClick.click();
-  //   expect(radioInputClick).toBeChecked();
-  //   expect(radioButton.radioCheckedFlag).toBeTruthy();
-  //   const contentAfterClick = getByTestId('radio-button-card-checked-content');
-  //   expect(contentAfterClick).toHaveStyle({
-  //     backgroundColor: window.getComputedStyle(document.body).getPropertyValue('--neutral'),
-  //     display: 'block'
-  //   })
-  // });
-
 });
