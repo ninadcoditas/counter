f8e99d42045bed2414bcf9ec24e71d119a0867d7
ninad <ninad.virkar@coditas.com>
2020-09-24 13:12:27 +0530

    refactor component logic

--- packages/counter/src/orxe-counter.ts
+++ packages/counter/src/orxe-counter.ts
@@ -6,14 +6,12 @@ export default class OrxeCounter extends LitElement {
 
   constructor() {
     super();
-
     let value = this.getAttribute("value");
     this.value = value == null ? 0 : parseInt(value.toString())
     let min = this.getAttribute("min")
     this.min = min == null ? 0 : parseInt(min.toString())
     let max = this.getAttribute("max")
     this.max = max == null ? 0 : parseInt(max.toString())
-
   }
 
   /**
@@ -41,71 +39,46 @@ export default class OrxeCounter extends LitElement {
   * Implement `render` to define a template for button element.
   */
   render() {
-
     return html`
   <div class="counter">
-
   ${this.innerHTML.trim() != "" ?
         html` 
         <div class="counter_label"> 
           <slot></slot>
           </div>` :
         html``}
-
     <div class="counter_container">
-    <div class="counter_element">
-    <button @click="${this.decrement}" id="decrement" ?disabled="${this.value <= this.min}"> 
-    - 
-    </button>
-    </div>  
-    <div class="counter_element" id="value">${this.value}</div>
-    <div class="counter_element">
-    <button @click="${this.increment}" id="increment" ?disabled="${this.value >= this.max}">
-    + 
-    </button>
-    </div>
-    </div>
+      <div class="counter_element">
+        <button @click="${() => this.value--}" id="decrement" ?disabled="${this.value <= this.min}"> 
+          - 
+        </button>
+      </div>  
+      <div class="counter_element" id="value">${this.value}</div>
+      <div class="counter_element">
+        <button @click="${() => this.value++}" id="increment" ?disabled="${this.value >= this.max}">
+          + 
+        </button>
+      </div>
+      </div>
     </div>
     `;
-
-  }
-
-
-  /**
-   * This method handles decrement
-   */
-  decrement() {
-    this.value--;
-    this.handleEdgeCase();
   }
 
   /**
-   * This method handles increment
+   * 
+   * updated lifecycle hook to keep track of value and emit event
    */
-  increment() {
-    this.value++
-    this.handleEdgeCase()
+
+  updated(changedProperties) {
+    console.log(changedProperties); // logs previous values
+    this.emitEvent()
   }
+
   /**
-   * This method handles edge case
+   * This method handles emits the value of counter
    */
 
-  handleEdgeCase() {
-    let incrementButton = (<HTMLInputElement>this.shadowRoot!.getElementById("increment"))
-    let decrementButton = (<HTMLInputElement>this.shadowRoot!.getElementById("decrement"))
-    if (this.value >= this.max) {
-      incrementButton.disabled = true;
-    }
-    else {
-      incrementButton.disabled = false;
-    }
-    if (this.value <= this.min) {
-      decrementButton.disabled = true;
-    }
-    else {
-      decrementButton.disabled = false;
-    }
-
+  emitEvent() {
     let event = new CustomEvent('value-changed', {
       detail: {
         value: this.value
